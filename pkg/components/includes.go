9c56c706fa1add5a7c470e4c438ef505c8bfa4d4
jaredpatrick37 <105118510+jaredpatrick37@users.noreply.github.com>
2022-06-10 16:31:29 -0400

    fix: add components to includes

--- pkg/components/includes.go
+++ pkg/components/includes.go
@@ -4,11 +4,13 @@ package components
 import (
 	"github.com/trustacks/catalog/pkg/components/concourse"
 	"github.com/trustacks/catalog/pkg/components/flux2"
-	_ "github.com/trustacks/catalog/pkg/components/minio"
-	_ "github.com/trustacks/catalog/pkg/components/vault"
+	"github.com/trustacks/catalog/pkg/components/minio"
+	"github.com/trustacks/catalog/pkg/components/vault"
 )
 
 func init() {
 	concourse.Initialize()
 	flux2.Initialize()
+	minio.Initialize()
+	vault.Initialize()
 }

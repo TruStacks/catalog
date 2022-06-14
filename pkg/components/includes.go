cfd514a97f936635121e509c4484e6b984435c88
jaredpatrick37 <105118510+jaredpatrick37@users.noreply.github.com>
2022-06-13 23:43:32 -0400

    feat: add sealedsecrets

--- pkg/components/includes.go
+++ pkg/components/includes.go
@@ -5,6 +5,7 @@ import (
 	"github.com/trustacks/catalog/pkg/components/concourse"
 	"github.com/trustacks/catalog/pkg/components/flux2"
 	"github.com/trustacks/catalog/pkg/components/minio"
+	"github.com/trustacks/catalog/pkg/components/sealedsecrets"
 	"github.com/trustacks/catalog/pkg/components/vault"
 )
 
@@ -12,5 +13,6 @@ func init() {
 	concourse.Initialize()
 	flux2.Initialize()
 	minio.Initialize()
+	sealedsecrets.Initialize()
 	vault.Initialize()
 }

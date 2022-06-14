b3904d9e8444078d9138af5ed1027dc8402f8eba
jaredpatrick37 <105118510+jaredpatrick37@users.noreply.github.com>
2022-06-13 23:00:50 -0400

    feat: add sealed secrets component

---
+++ pkg/components/sealedsecrets/sealedsecrets.go
@@ -0,0 +1,32 @@
+package sealedsecrets
+
+import (
+	"log"
+
+	"github.com/trustacks/catalog/pkg/catalog"
+)
+
+const componentName = "sealedsecrets"
+
+type sealedsecrets struct {
+	catalog.BaseComponent
+}
+
+// Initialize adds the component to the catalog and configures hooks.
+func Initialize() {
+	config, err := catalog.LoadComponentConfig(componentName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	component := &sealedsecrets{
+		catalog.BaseComponent{
+			Repo:       config.Repo,
+			Chart:      config.Chart,
+			Version:    config.Version,
+			Values:     config.Values,
+			Hooks:      config.Hooks,
+			Parameters: config.Parameters,
+		},
+	}
+	catalog.AddComponent(componentName, component)
+}

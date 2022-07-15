df49bea8f3ee78f15e76a0885032be40a6eac64f
Jared Patrick <jpatrick@cornerstonets.net>
2022-07-15 10:50:14 -0400

    ci: remove file creation prerequisites

--- .devcontainer/docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# link data assets for local run and test.
-sudo mkdir -p /data
-sudo mkdir -p /var/run/secrets/kubernetes.io/serviceaccount
-sudo chown $(id -u):$(id -g) /var/run/secrets/kubernetes.io/serviceaccount
-sudo ln -s $PWD/pkg/components /data/components
-sudo ln -s $PWD/pkg/catalog.yaml /data/config.yaml

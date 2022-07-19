f28cd47b054009cda1d879505ad58ffdddf423c7
Jared Patrick <jpatrick@cornerstonets.net>
2022-07-19 11:20:30 -0400

    feat: add argo-cd hooks

--- pkg/components/argocd/component.go
+++ pkg/components/argocd/component.go
@@ -1,22 +1,102 @@
 package argocd
 
 import (
+	"context"
 	_ "embed"
+	"errors"
+	"io/ioutil"
 	"log"
+	"os"
+	"strings"
 
 	"github.com/trustacks/catalog/pkg/catalog"
+	"github.com/trustacks/catalog/pkg/functions"
 	"github.com/trustacks/catalog/pkg/hooks"
 	"gopkg.in/yaml.v2"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 )
 
-const componentName = "argocd"
+const (
+	// componentName is the name of the component.
+	componentName = "argo-cd"
+	// inClusterNamespace is the path to the in-cluster namespace.
+	inClusterNamespace = "/var/run/secrets/kubernetes.io/serviceaccount/namespace"
+)
 
 type argocd struct {
 	catalog.BaseComponent
 }
 
+// preInstall creates the oidc client and secret.
+func (c *argocd) preInstall() error {
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		return err
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return err
+	}
+	namespace, err := getNamespace()
+	if err != nil {
+		return err
+	}
+	provider := os.Getenv("SSO_PROVIDER")
+	clientId, clientSecret, err := createOIDCClient(provider)
+	if err != nil {
+		return err
+	}
+	return createOIDCClientSecret(clientId, clientSecret, namespace, clientset)
+}
+
+// createOIDCClient creates the concourse oidc client.
+func createOIDCClient(provider string) (string, string, error) {
+	result, err := functions.Call("create-oidc-client", map[string]interface{}{
+		"name":     componentName,
+		"provider": provider,
+	})
+	if err != nil {
+		return "", "", err
+	}
+	v, ok := result.(map[string]interface{})
+	if !ok {
+		return "", "", errors.New("error parsing the result")
+	}
+	return v["clientId"].(string), v["clientSecret"].(string), nil
+}
+
+// createOIDCClientSecret creates the oidc client secret.
+func createOIDCClientSecret(clientId, clientSecret, namespace string, clientset kubernetes.Interface) error {
+	secret := &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "oidc-client",
+		},
+		Data: map[string][]byte{
+			"id":     []byte(clientId),
+			"secret": []byte(clientSecret),
+		},
+	}
+	_, err := clientset.CoreV1().Secrets(namespace).Create(context.TODO(), secret, metav1.CreateOptions{})
+	return err
+}
+
+// getNamespace gets the current kubernetes namespace.
+func getNamespace() (string, error) {
+	data, err := ioutil.ReadFile(inClusterNamespace)
+	if err != nil {
+		return "", err
+	}
+	return strings.TrimSpace(string(data)), err
+}
+
 //go:embed config.yaml
 var config []byte
+
+//go:embed hooks.yaml
+var hookManifests []byte
 
 // Initialize adds the component to the catalog and configures hooks.
 func Initialize(c *catalog.ComponentCatalog) {
@@ -30,12 +110,14 @@ func Initialize(c *catalog.ComponentCatalog) {
 			Chart:   conf.Chart,
 			Version: conf.Version,
 			Values:  conf.Values,
-			Hooks:   "",
+			Hooks:   string(hookManifests),
 		},
 	}
 	c.AddComponent(componentName, component)
 
-	for hook, fn := range map[string]func() error{} {
+	for hook, fn := range map[string]func() error{
+		hooks.PreInstallHook: component.preInstall,
+	} {
 		if err := hooks.AddHook(componentName, hook, fn); err != nil {
 			log.Fatal(err)
 		}
--- pkg/components/argocd/component_test.go
+++ pkg/components/argocd/component_test.go
@@ -1,12 +1,17 @@
 package argocd
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/trustacks/catalog/pkg/catalog"
+	"github.com/trustacks/catalog/pkg/functions"
 	"gopkg.in/yaml.v3"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes/fake"
 )
 
 func TestGetChart(t *testing.T) {
@@ -22,4 +27,34 @@ func TestGetChart(t *testing.T) {
 	if resp.StatusCode != 200 {
 		t.Fatal("failed retrieving the helm chart")
 	}
+}
+
+func TestCreateOIDCClient(t *testing.T) {
+	var p map[string]interface{}
+	defer functions.PatchMockFunction("create-oidc-client", func(params map[string]interface{}) (interface{}, error) {
+		fmt.Println("called")
+		p = params
+		return map[string]interface{}{"clientId": "test-id", "clientSecret": "test-secret"}, nil
+	})()
+	clientId, clientSecret, err := createOIDCClient("test-provider")
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, componentName, p["name"], "got an unexpected provider")
+	assert.Equal(t, "test-provider", p["provider"], "got an unexpected provider")
+	assert.Equal(t, "test-id", clientId, "got an unexpected client id")
+	assert.Equal(t, "test-secret", clientSecret, "got an unexpected client secret")
+}
+
+func TestCreateOIDCClientSecret(t *testing.T) {
+	clientset := fake.NewSimpleClientset()
+	if err := createOIDCClientSecret("test-id", "test-secret", "test", clientset); err != nil {
+		t.Fatal(err)
+	}
+	secret, err := clientset.CoreV1().Secrets("test").Get(context.TODO(), "oidc-client", metav1.GetOptions{})
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, "test-id", string(secret.Data["id"]), "got an unexpected client id")
+	assert.Equal(t, "test-secret", string(secret.Data["secret"]), "got an unexpected client secret")
 }
